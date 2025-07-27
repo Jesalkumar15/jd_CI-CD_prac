daa6bb46c54f05a7471aef07bc47f4829d2544b1
Jesalkumar15 <jesaldesai92@gmail.com>
2025-07-27 16:14:57 -0500

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull jesaldesai92/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 docker jesaldesai92/simple-python-flask-app
+docker run -d -p 5001:5001 docker jesaldesai92/simple-python-flask-app
